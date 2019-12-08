0c9d9d8665dc1923110a51aedefac1417d5d09f5
Emerson Silva <emerson@mac.local>
2019-12-08 12:59:51 -0300

    ship

--- src/pages/Terminal/Map/ships.js
+++ src/pages/Terminal/Map/ships.js
@@ -19,6 +19,46 @@ const ships = [
     },
     img: 'https://photos.marinetraffic.com/ais/showphoto.aspx?photoid=404196'
   },
+  {
+    id: Math.random(),
+    name: 'MAMA LIAN HAIRIA',
+    type: 'Bulk Carrier',
+    IMO: 9577422,
+    MMSI: 636014652,
+    Indicativo: 'A8VQ5',
+    Bandeira: 'Panama [PA]',
+    ship_type: 'Cargo',
+    ton: 34335,
+    port: '58407t',
+    length: '196m × 32.25m',
+    construction: '2011',
+    state: 'Ativo',
+    cords: {
+      lat: '-24.05586',
+      long: '-46.3334°',
+    },
+    img: 'https://photos.marinetraffic.com/ais/showphoto.aspx?photoid=3327190'
+  },
+  {
+    id: Math.random(),
+    name: 'MAMA LIAN HAIRIA',
+    type: 'Bulk Carrier',
+    IMO: 9577422,
+    MMSI: 636014652,
+    Indicativo: 'A8VQ5',
+    Bandeira: 'Panama [PA]',
+    ship_type: 'Cargo',
+    ton: 34335,
+    port: '58407t',
+    length: '196m × 32.25m',
+    construction: '2011',
+    state: 'Ativo',
+    cords: {
+      lat: '-24.09608',
+      long: '-46.33044',
+    },
+    img: 'https://photos.marinetraffic.com/ais/showphoto.aspx?photoid=3327190'
+  },
 ];
 
 export default ships;
