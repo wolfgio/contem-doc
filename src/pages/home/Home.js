c811e621a34a2c814ec6ffc3da9b81decc0ec3bf
Emerson Silva <emerson@mac.local>
2019-12-08 04:37:44 -0300

    feat(documents) : change on screen to enable table

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,97 @@
-import * as React from 'react';
-import { Typography } from 'antd';
+import * as React from "react";
+import { Typography, Row, Col , Table, Divider, Tag} from "antd";
 
-import Scaffold from '../../components/scaffold';
+import Scaffold from "../../components/scaffold";
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: tags => (
+      <span>
+        {tags.map(tag => {
+          let color = tag.length > 5 ? 'geekblue' : 'green';
+          if (tag === 'Aguardando Documentação') {
+            color = 'volcano';
+          }
+          if (tag === 'Documentos Ok') {
+            color = 'lightgreen';
+          }
+          if (tag === 'Data em atraso') {
+            color = 'yellow';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <a>Invite {record.name}</a>
+        <Divider type="vertical" />
+        <a>Delete</a>
+      </span>
+    ),
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['Aguardando Documentação'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['Documentos Ok'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['Data em atraso'],
+  },
+];
+
 
 const Home = () => (
   <Scaffold>
-    <Typography.Title>Oi</Typography.Title>
+    <Typography.Title>Terminais</Typography.Title>
+    <Row>
+      <Col span={12}>
+      <Table columns={columns} dataSource={data} />
+      </Col>
+    </Row>
   </Scaffold>
 );
 
